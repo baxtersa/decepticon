59901d8148b9d94abc14346f24abfc50b08129a9
Sam Baxter <sbaxter@cs.umass.edu>
2019-09-07 16:26:41 -0400

    Add some math fns needed for backprop

--- src/nn/math.rs
+++ src/nn/math.rs
@@ -1,3 +1,25 @@
 pub fn sigmoid(x: f64) -> f64 {
     return 1.0 / (1.0 + (-x).exp());
 }
+
+pub fn deriv_sigmoid(x: f64) -> f64 {
+    let fx = sigmoid(x);
+    return fx * (1.0 - fx);
+}
+
+pub fn mse(expecteds: &[f64], actuals: &[f64]) -> f64 {
+    let sum_squared_errors: f64 = expecteds
+        .iter()
+        .zip(actuals.iter())
+        .map(|(x, y)| (x - y).powf(2.0))
+        .sum();
+    return sum_squared_errors / expecteds.len() as f64;
+}
+
+#[test]
+fn test_mse() {
+    let expecteds = [0.0, 0.0, 0.0, 0.0];
+    let actuals = [1.0, 0.0, 0.0, 1.0];
+
+    assert_eq!(0.5, mse(&expecteds, &actuals))
+}
